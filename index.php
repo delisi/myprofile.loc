ec9e9f392d84ec9303e3ebbc0dedf7d3f50b0e65
Andrey <070919872008@mail.ru>
2012-08-13 23:59:28 +0300

    add new application

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/yii/framework/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
@@ -10,5 +10,4 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
-
 Yii::createWebApplication($config)->run();
